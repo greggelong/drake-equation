c27684c5190a8f553c054c408b39fdd7e050e24a
Greg K <gkreisman_cep@qq.com>
2019-01-19 21:46:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Random numbers for the Drake equation
+## visualized with turtle graphics
+
+### Random numbers for Drake
+ 
+*(R), the average rate of star formations, in our galaxy is assumed to be 7 per year 
+*(p), the percentage of formed stars that have planets 100% 1.0
+*(n)for stars that have planets, the average number of planets that can potentially support life is, 6
+*(l), the percentage of those planets that actually develop life, 94% 0.94
+*(c), the percentage of these civilizations that have developed communications, i.e., technologies that release detectable signs into space,  24% 0.24
+*(L), the length of time over which such civilizations release detectable signals,  598  years.
+*N = 3456.3634559999996
---
+++ drakespeak.py
@@ -0,0 +1,131 @@
+import turtle
+import random
+import os
+
+wn = turtle.Screen()
+
+r_stars = turtle.Turtle()
+p_planets = turtle.Turtle()
+n_planetwithlife=turtle.Turtle()
+l_life=turtle.Turtle()
+i_intellegent = turtle.Turtle()
+c_communicate = turtle.Turtle()
+L_civlength = turtle.Turtle()
+N_discoverable = turtle.Turtle()
+
+
+wn.setworldcoordinates(0,0,1000,1000)
+turtle.tracer(1,0) #deflt tracer is (1,10) the second part is animation speed
+wn.bgcolor("grey")
+
+
+r_stars.color("yellow")
+p_planets.color("brown")
+n_planetwithlife.color("green")
+l_life.color("purple")
+i_intellegent.color("black")
+c_communicate.color("white")
+L_civlength.color("red")
+N_discoverable.color("white")
+
+N_discoverable.speed(0)
+r_stars.pensize(5)
+p_planets.pensize(5)
+n_planetwithlife.pensize(5)
+l_life.pensize(5)
+i_intellegent.pensize(5)
+c_communicate.pensize(5)
+L_civlength.pensize(5)
+N_discoverable.pensize(1)
+
+N_discoverable.penup()
+N_discoverable.shape("circle")
+N_discoverable.ht()
+
+def speakout(say):
+    os.system('espeak "{}"'.format(say))#interpolate the string that you want to speak into your command.
+    
+    
+def getRandDrake():
+	print("*"*24)
+	print("Random numbers for Drake")
+	os.system('espeak "random numbers for the drake equation"')
+	print("*"*24)
+	print("(R), the average rate of star formations, in our galaxy is assumed to be 7 per year ")
+	os.system('espeak "(R), the average rate of star formations, in our galaxy is assumed to be 7 per year "')
+	r_stars.goto(100,100)
+	r_stars.begin_fill()
+	r_stars.circle(35)
+	r_stars.end_fill()
+	
+	
+	p = random.randint(1,100)
+	print("(p), the percentage of formed stars that have planets", str(p)+"%", p/100)
+	os.system('espeak "(p), the percentage of formed stars that have planets"')
+	os.system('espeak "{} percent"'.format(p))
+	p_planets.goto(200,200)
+	p_planets.begin_fill()
+	p_planets.circle(p)
+	p_planets.end_fill()
+	
+	n= random.randint(0,50)
+	print("(n)for stars that have planets, the average number of planets that can potentially support life is,",n)
+	os.system('espeak "(n) for stars that have planets, the average number of planets that can potentially support life is"')
+	os.system('espeak "{} planets"'.format(n))
+	n_planetwithlife.goto(300,300)
+	n_planetwithlife.begin_fill()
+	n_planetwithlife.circle(n)
+	n_planetwithlife.end_fill()
+	
+	l= random.randint(0,100)
+	print("(l), the percentage of those planets that actually develop life,",str(l)+"%",l/100)
+	os.system('espeak "(l), the percentage of those planets that actually develop life," ')
+	os.system('espeak "{} percent"'.format(l))
+	l_life.goto(400,400)
+	l_life.begin_fill()
+	l_life.circle(l)
+	l_life.end_fill()
+	
+	i = random.randint(0,100)
+	print("(i), the percentage of planets bearing life on which intelligent,  life, has developed, ",str(i)+"%",i/100)
+	os.system('espeak  "(i), the percentage of planets bearing life on which intelligent,  life, has developed, " ')
+	os.system('espeak "{} percent"'.format(i))
+	i_intellegent.goto(500,500)
+	i_intellegent.begin_fill()	
+	i_intellegent.circle(i)
+	i_intellegent.end_fill()
+	
+	c = random.randint(0,100)
+	print("(c), the percentage of these civilizations that have developed communications, i.e., technologies that release detectable signs into space, ",str(c)+"%",c/100)
+	os.system('espeak "(c), the percentage of these civilizations that have developed communications, i.e., technologies that release detectable signs into space, "  ')
+	os.system('espeak "{} percent"'.format(c))
+	c_communicate.goto(600,600)
+	c_communicate.begin_fill()
+	c_communicate.circle(c)
+	c_communicate.end_fill()
+	
+	
+	lenciv = random.randint(200,1000)
+	print("(L), the length of time over which such civilizations release detectable signals, ",lenciv," years.")
+	os.system('espeak "(L), the length of time over which such civilizations release detectable signals, "  ')
+	os.system('espeak "{} years"'.format(lenciv))
+	L_civlength.goto(700,700)
+	L_civlength.begin_fill()
+	L_civlength.circle(lenciv/10)
+	L_civlength.end_fill()
+	
+	numDeCiv = 7*(p/100)*n*(l/1/100)*(i/100)*(c/100)*lenciv
+	print("N =", numDeCiv)
+	os.system('espeak "thus, N equals {} alien civilizations"'.format(int(numDeCiv)))
+	
+	for aliens in range(int(numDeCiv)):
+		x = random.randint(0,1000)
+		y = random.randint(0,1000)
+		N_discoverable.goto(x,y)
+		#N_discoverable.stamp()  # used on pydroid
+		N_discoverable.dot()
+	return numDeCiv
+	
+getRandDrake()
+
+wn.exitonclick()
